eb0dccb54e480b4b163f165e9394c04af50a7a3d
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-11-25 02:10:27 -0800

    adds command line arguments

--- openFrameworks/NotTooPublic/src/main.cpp
+++ openFrameworks/NotTooPublic/src/main.cpp
@@ -4,14 +4,16 @@
 #include "ofAppGlutWindow.h"
 
 //========================================================================
-int main( ){
+int main(int argc, char* argv[]){
 
     ofAppGlutWindow window;
+	// can be OF_WINDOW or OF_FULLSCREEN
 	ofSetupOpenGL(&window, 900,700, OF_FULLSCREEN);
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new Secret());
-
+    if((argc > 1) && !string(argv[1]).compare("-b"))
+        ofRunApp( new Bling());
+    else if((argc > 1) && !string(argv[1]).compare("-s"))
+        ofRunApp( new Secret());
+    else
+        ofRunApp( new Secret());
 }
