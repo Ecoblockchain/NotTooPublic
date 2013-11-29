2798a7963bf125227e7026c86b15ece4ebf9e5da
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-11-28 16:45:22 -0800

    fixes indent

--- openFrameworks/NotTooPublic/src/Wish.h
+++ openFrameworks/NotTooPublic/src/Wish.h
@@ -3,14 +3,14 @@
 #include "NotTooPublic.h"
 
 class Wish : public NotTooPublic{
-public:
-    void setup();
-    void update();
-    void draw();
-private:
-    const int FADE_DELTA = 5;
-    const string myFontName = "cooperBlack.ttf";
-    string currentMessage;
-    vector<ofPath> currentMessagePath;
-    float currentDistance, originalDistance;
+    public:
+        void setup();
+        void update();
+        void draw();
+    private:
+        const int FADE_DELTA = 5;
+        const string myFontName = "cooperBlack.ttf";
+        string currentMessage;
+        vector<ofPath> currentMessagePath;
+        float currentDistance, originalDistance;
 };
